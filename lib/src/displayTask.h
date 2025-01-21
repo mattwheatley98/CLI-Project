a573ef22ccc98f89bb5b1d3e991f91f2f4d489d9
mattwheatley98 <113391095+mattwheatley98@users.noreply.github.com>
2025-01-21 10:24:35 -0500

    Update displayTask.h

--- lib/src/displayTask.h
+++ lib/src/displayTask.h
@@ -2,10 +2,7 @@
 // Created by matt on 4/7/23.
 //
 
-#ifndef CLIPROJECT_DISPLAYTASK_H
-#define CLIPROJECT_DISPLAYTASK_H
-
-#endif //CLIPROJECT_DISPLAYTASK_H
+#pragma once
 
 void displayTask(void *parameter);
 extern QueueHandle_t displayQueue;
